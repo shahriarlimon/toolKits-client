3d9ab9658e33a1acb1c003108fc5f3f90d004ab8
shahriarlimon <shahriarwebdev@gmail.com>
2022-05-23 00:37:06 +0600

    installed firebase and implemented basic set up

--- src/Components/Pages/Home/Carosel.js
+++ src/Components/Pages/Home/Carosel.js
@@ -7,28 +7,28 @@ const Carosel = () => {
         <div id="item1" class="carousel-item w-full ">
           <img
             src="https://wallpaperaccess.com/full/1880033.jpg"
-            class="w-full h-[400px]"
+            class="w-full h-[500px]"
             alt=""
           />
         </div>
         <div id="item2" class="carousel-item w-full">
           <img
             src="https://wallpaperaccess.com/full/1880053.jpg"
-            class="w-full h-[400px]"
+            class="w-full h-[500px]"
             alt=""
           />
         </div>
         <div id="item3" class="carousel-item w-full">
           <img
             src="https://c1.wallpaperflare.com/preview/381/886/420/tools-work-repair-hammer-thumbnail.jpg"
-            class="w-full h-[400px]"
+            class="w-full h-[500px]"
             alt=""
           />
         </div>
         <div id="item4" class="carousel-item w-full">
           <img
             src="https://blog.dba.bg/media/blogs/RStanoev/quick-uploads/p157/psu.jpg?mtime=1489659044"
-            class="w-full h-[400px]"
+            class="w-full h-[500px]"
             alt=""
           />
         </div>
--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ({children}) => {
         <input id="my-drawer-3" type="checkbox" class="drawer-toggle" /> 
         <div class="drawer-content flex flex-col">
          {/*  <!-- Navbar --> */}
-          <div class="w-full navbar bg-base-300">
+          <div class="w-full navbar bg-white">
           <div class="flex-1 px-2 mx-2 text-2xl">ToolKits</div>
             <div class="flex-none lg:hidden">
               <label for="my-drawer-3" class="btn btn-square btn-ghost">
